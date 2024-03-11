c8c7e9a8b97a876dda1030e4ee44554205e20e1c
typ49 <typ490@gmail.com>
2024-03-11 17:01:08 +0100

    init project'

--- build/testFormatPrint[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/juliengauthier410/l3/s6/programmation_Multi-Paradigme/projet_3/format/build/testFormatPrint[1]_tests.cmake")
-  include("/home/juliengauthier410/l3/s6/programmation_Multi-Paradigme/projet_3/format/build/testFormatPrint[1]_tests.cmake")
-else()
-  add_test(testFormatPrint_NOT_BUILT testFormatPrint_NOT_BUILT)
-endif()
--- build/testFormatPrint[1]_tests.cmake
+++
@@ -1,1 +0,0 @@
-set(  testFormatPrint_TESTS)
