66347761af5ff4c3b61e7ca4218f352bb9c33fd3
typ49 <typ490@gmail.com>
2024-03-11 16:57:36 +0100

    init project

---
+++ Format.h
@@ -0,0 +1,18 @@
+#ifndef FP_FORMAT_H
+#define FP_FORMAT_H
+
+#include <string>
+
+namespace fp {
+
+  /**
+   * @brief Insert all parameters into a formatted string
+   */
+  std::string format(const std::string& formatString /* implementation defined */) {
+    return formatString;
+  }
+
+}
+
+
+#endif // FP_FORMAT_H
---
+++ build/testFormatPrint[1]_tests.cmake
@@ -0,0 +1,1 @@
+set(  testFormatPrint_TESTS)
